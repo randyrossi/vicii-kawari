697fe6676b1c4a8947a21e783e7758a9d28b05a1
Randy Rossi <randy.rossi@gmail.com>
2020-04-25 00:23:05 -0400

    Add ability to see video in a SDL window
    Also added command line args to control
    when to start capturing to either window or vcd file.

--- vicii.srcs/sources_1/new/sim_main.cpp
+++ vicii.srcs/sources_1/new/sim_main.cpp
@@ -1,7 +1,11 @@
+#include <SDL2/SDL.h>
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+#include <iostream>
 
 // Include common routines
 #include <verilated.h>
@@ -45,6 +49,8 @@ unsigned char prev_signal_values[NUM_SIGNALS];
    (*signal_src[signum] & signal_bit[signum] ? 1 : 0)
 #define HASCHANGED(signum) \
    ( signal_monitor[signum] && GETVAL(signum) != prev_signal_values[signum] )
+#define RISING(signum) \
+   ( signal_monitor[signum] && GETVAL(signum))
 
 // Convert microseconds to ticks (picoseconds)
 #define US_TO_TICKS(t) (t * 1000L * 1000L)
@@ -67,7 +73,7 @@ static vluint64_t nextTick(Vtop* top) {
       nextClk1 += 15275;
       top->top__DOT__clk_dot4x = ~top->top__DOT__clk_dot4x;
       return ticks + diff1;
-   } 
+   }
    nextClk2 += 34920;
    top->top__DOT__clk_col4x = ~top->top__DOT__clk_col4x;
    return ticks + diff2;
@@ -108,27 +114,37 @@ static void vcd_header(Vtop* top) {
 
 int main(int argc, char** argv, char** env) {
     bool capture = false;
+    bool show_vcd = false;
+    bool show_window = false;
+    int prev_y = -1;
 
-    // Default to 20ms starting at 0
+    // Default to 16.7us starting at 0
     startTicks = US_TO_TICKS(0);
-    endTicks = US_TO_TICKS(20000L);
+    vluint64_t durationTicks = US_TO_TICKS(16700L);
 
-    char *cvalue = NULL;
+    char *cvalue = nullptr;
     char c;
 
-    while ((c = getopt (argc, argv, "s:t:")) != -1)
-    switch (c)
-      {
+    while ((c = getopt (argc, argv, "s:t:vw")) != -1)
+    switch (c) {
+      case 'v':
+        show_vcd = true;
+        break;
+      case 'w':
+        show_window = true;
+        break;
       case 's':
         startTicks = US_TO_TICKS(atol(optarg));
         break;
       case 't':
-        endTicks = startTicks + US_TO_TICKS(atol(optarg));
+        durationTicks = US_TO_TICKS(atol(optarg));
         break;
       case 'h':
         printf ("Usage\n");
-        printf ("  -s start uS\n");
-        printf ("  -t duration uS\n");
+        printf ("  -s [uS] : start at uS\n");
+        printf ("  -t [uS] : run for uS\n");
+        printf ("  -v      : generate vcd to stdout\n");
+        printf ("  -w      : show SDL2 window\n");
         exit(0);
       case '?':
         if (optopt == 't' || optopt == 's')
@@ -142,7 +158,45 @@ int main(int argc, char** argv, char** env) {
         return 1;
       default:
         abort ();
+    }
+
+    endTicks = startTicks + durationTicks;
+
+    int sdl_init_mode = SDL_INIT_VIDEO;
+    if (SDL_Init(sdl_init_mode) != 0) {
+      std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+      return 1;
+    }
+
+    SDL_Event event;
+    SDL_Renderer* ren = nullptr;
+    SDL_Window* win;
+
+    if (show_window) {
+      SDL_DisplayMode current;
+      int width = 520; // NTSC
+      int height = 262; // NTSC
+
+      win = SDL_CreateWindow("VICII",
+                             SDL_WINDOWPOS_CENTERED,
+                             SDL_WINDOWPOS_CENTERED,
+                             width, height, SDL_WINDOW_SHOWN);
+      if (win == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
       }
+
+      ren = SDL_CreateRenderer(
+          win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+      if (ren == nullptr) {
+        std::cerr << "SDL_CreateRenderer Error: "
+           << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+      }
+    }
 
     // Add new input/output here.
     Vtop* top = new Vtop;
@@ -162,35 +216,32 @@ int main(int argc, char** argv, char** env) {
     }
 
     // Add new input/output here.
-    signal_src[OUT_PHI] = &top->clk_phi;   
-    signal_src[OUT_COLREF] = &top->clk_colref;   
-    signal_src[IN_RST] = &top->rst;   
-    signal_src[OUT_R0] = &top->red;   
-    signal_src[OUT_R1] = &top->red;   
+    signal_src[OUT_PHI] = &top->clk_phi;
+    signal_src[OUT_COLREF] = &top->clk_colref;
+    signal_src[IN_RST] = &top->rst;
+    signal_src[OUT_R0] = &top->red;
+    signal_src[OUT_R1] = &top->red;
     signal_bit[OUT_R1] = 2;
-    signal_src[OUT_G0] = &top->green;   
-    signal_src[OUT_G1] = &top->green;   
+    signal_src[OUT_G0] = &top->green;
+    signal_src[OUT_G1] = &top->green;
     signal_bit[OUT_G1] = 2;
-    signal_src[OUT_B0] = &top->blue;   
-    signal_src[OUT_B1] = &top->blue;   
+    signal_src[OUT_B0] = &top->blue;
+    signal_src[OUT_B1] = &top->blue;
     signal_bit[OUT_B1] = 2;
-    signal_src[OUT_DOT] = &top->top__DOT__clk_dot;   
-    signal_src[OUT_CSYNC] = &top->cSync;   
+    signal_src[OUT_DOT] = &top->top__DOT__clk_dot;
+    signal_src[OUT_CSYNC] = &top->cSync;
 
     for (int i = 0; i < NUM_SIGNALS; i++) {
        prev_signal_values[i] = GETVAL(i);
     }
 
-    vcd_header(top);
+    if (show_vcd)
+       vcd_header(top);
 
     // Simulate until $finish
     while (!Verilated::gotFinish()) {
         // Advance simulation time. Each tick represents 1 picosecond.
         ticks = nextTick(top);
-
-        // Toggle simulated system clock. One sys_clock
-        // period represents 2 picoseconds duration.
-        //top->sys_clock = !top->sys_clock;
 
 #ifdef TEST_RESET
         // Test reset between approx 30 and approx 40 us
@@ -203,7 +254,7 @@ int main(int argc, char** argv, char** env) {
         // Evaluate model
         top->eval();
 
-        capture = (ticks >= startTicks) && (ticks <=endTicks);
+        capture = (ticks >= startTicks) && (ticks <= endTicks);
         if (capture) {
           bool anyChanged = false;
           for (int i = 0; i < NUM_SIGNALS; i++) {
@@ -211,16 +262,40 @@ int main(int argc, char** argv, char** env) {
                 anyChanged = true;
                 break;
              }
-          }      
+          }
 
           if (anyChanged) {
-             printf ("#%" VL_PRI64 "d\n", ticks/PICOSECONDS_TO_TIMESCALE);
+             if (show_vcd)
+                printf ("#%" VL_PRI64 "d\n", ticks/PICOSECONDS_TO_TIMESCALE);
              for (int i = 0; i < NUM_SIGNALS; i++) {
-                if (HASCHANGED(i))
-                   printf ("%x%s\n", GETVAL(i), signal_ids[i]);
+                if (HASCHANGED(i)) {
+                   if (show_vcd)
+                      printf ("%x%s\n", GETVAL(i), signal_ids[i]);
+
+                }
              }
           }
+
+        // If rendering, draw current color on dot clock
+        if (show_window && HASCHANGED(OUT_DOT) && RISING(OUT_DOT)) {
+           SDL_SetRenderDrawColor(ren,
+              top->red << 6,
+              top->green << 6,
+              top->blue << 6,
+              255);
+           SDL_RenderDrawPoint(ren,
+              top->top__DOT__vic_inst__DOT__x_pos,
+              top->top__DOT__vic_inst__DOT__y_pos);
+
+           if (prev_y != top->top__DOT__vic_inst__DOT__y_pos) {
+              SDL_RenderPresent(ren);
+              prev_y = top->top__DOT__vic_inst__DOT__y_pos;
+
+              SDL_PollEvent(&event);
+           }
         }
+        }
+
 
         for (int i = 0; i < NUM_SIGNALS; i++) {
            prev_signal_values[i] = GETVAL(i);
@@ -228,6 +303,26 @@ int main(int argc, char** argv, char** env) {
 
         if (ticks >= endTicks)
            break;
+
+    }
+
+    if (show_window) {
+       bool quit = false;
+       while (!quit) {
+          while (SDL_PollEvent(&event)) {
+             switch (event.type) {
+                case SDL_QUIT:
+                case SDL_KEYUP:
+                   quit=true; break;
+                default:
+                   break;
+             }
+           }
+       }
+
+       SDL_DestroyRenderer(ren);
+       SDL_DestroyWindow(win);
+       SDL_Quit();
     }
 
     // Final model cleanup
