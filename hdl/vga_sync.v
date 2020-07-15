26774ec80cd5a9b03c7f8ceb1ee97a3e8134348c
Randy Rossi <randy.rossi@gmail.com>
2020-07-15 18:17:33 -0400

    Make VGA raster/scaler same for NTSC and PAL
    No need for the differences between them.

--- hdl/vga_sync.v
+++ hdl/vga_sync.v
@@ -1,15 +1,14 @@
 `include "common.vh"
 
-// Double the native resolution of the C64 in both dimensions.
 
 // PAL - TODO Make regs
-localparam HS_STA = 21;              // horizontal sync start
-localparam HS_END = 21 + 116;        // horizontal sync end
-localparam HA_STA = 21 + 116 + 65;   // horizontal active pixel start
+localparam HS_STA = 16;              // horizontal sync start
+localparam HS_END = 16 + 48;        // horizontal sync end
+localparam HA_STA = 16 + 48 + 65;   // horizontal active pixel start
 localparam VS_STA = 569 + 11;        // vertical sync start
 localparam VS_END = 569 + 11 + 3;    // vertical sync end
 localparam VA_END = 569;             // vertical active pixel end
-localparam LINE   = 1007;            // complete line (pixels)
+localparam LINE   = 503;            // complete line (pixels)
 localparam SCREEN = 623;             // complete screen (lines)
 localparam VERTICAL_OFFSET = 63;
 localparam HORIZONTAL_OFFSET = 29;
@@ -59,25 +58,22 @@ module vga_sync(
         begin
             h_count <= 0;
             v_count <= SCREEN - VERTICAL_OFFSET; // TODO Make this a runtime param
-        end
-        else
-        begin
-            // NTSC : horiz is 1x native res
-            // PAL : horiz is 2x native res
-            if (!is_pal)
-               ff = ~ff;
-
+        end else begin
+            ff = ~ff;
+            // Increment x/y every other clock for a 2x dot clock in which
+            // only our Y dimension is doubled.  Each line from the line
+            // buffer is drawn twice.
             if (ff) begin
-            if (h_count < LINE) begin
-               h_count <= h_count + 1;
-            end else begin
-               h_count <= 0;
-               if (v_count < SCREEN) begin
-                  v_count <= v_count + 1;
-               end else begin
-                  v_count <= 0;
-               end
-            end
+                if (h_count < LINE) begin
+                   h_count <= h_count + 1;
+                end else begin
+                   h_count <= 0;
+                   if (v_count < SCREEN) begin
+                      v_count <= v_count + 1;
+                   end else begin
+                      v_count <= 0;
+                   end
+                end
             end
         end
     end
@@ -106,38 +102,28 @@ module vga_scaler(
 always @(posedge clk_dot4x)
 begin
    if (!rst) begin
-   if (dot_rising_0) begin
-      if (raster_x == 0)
-         active_buf = ~active_buf;
+      if (dot_rising_0) begin
+         if (raster_x == 0)
+            active_buf = ~active_buf;
 
-      // Store pixels into line buf
-      if (active_buf)
-        line_buf_0[raster_x[8:0]] = pixel_color3;
-      else
-        line_buf_1[raster_x[8:0]] = pixel_color3;
-   end
+         // Store pixels into line buf
+         if (active_buf)
+           line_buf_0[raster_x[8:0]] = pixel_color3;
+         else
+           line_buf_1[raster_x[8:0]] = pixel_color3;
+      end
    end
 end
-
-wire [8:0] h_count_div_2;
-assign h_count_div_2 = h_count[9:1];
 
 always @(posedge clk_dot4x)
 begin
    if (!rst) begin
-   if (h_count >= HORIZONTAL_OFFSET) begin
-      if (!is_pal) begin
-        // NTSC : horiz is 1x native res
-        pixel_color4 = !active_buf ? line_buf_0[h_count - HORIZONTAL_OFFSET] :
-                                     line_buf_1[h_count - HORIZONTAL_OFFSET];
-      end else begin
-        // PALC : horiz is 2x native res
-        pixel_color4 = !active_buf ? line_buf_0[h_count_div_2 - HORIZONTAL_OFFSET] :
-                                     line_buf_1[h_count_div_2 - HORIZONTAL_OFFSET];
-      end
-   end else
-        pixel_color4 = 4'b0;
+       if (h_count >= HORIZONTAL_OFFSET) begin
+           pixel_color4 = !active_buf ? line_buf_0[h_count - HORIZONTAL_OFFSET] :
+                                        line_buf_1[h_count - HORIZONTAL_OFFSET];
+       end else
+           pixel_color4 = 4'b0;
+       end
    end
-end
 
 endmodule : vga_scaler
