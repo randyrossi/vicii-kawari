765ee54e1601093d04e5351c2e3af6f28d62ca34
Randy Rossi <randy.rossi@gmail.com>
2020-09-13 20:38:54 -0400

    Verilog for a flip flop

--- hdl/redff.v
+++ hdl/redff.v
@@ -1,5 +1,7 @@
 `timescale 1ns/1ps
-
+//
+// https://www.fpga4student.com/2017/02/verilog-code-for-d-flip-flop.html
+//
 module RisingEdge_DFlipFlop_SyncReset(input D,input clk,input SR,output reg Q);
 always @(posedge clk)
 begin
@@ -9,3 +11,10 @@ begin
         Q <= D;
 end
 endmodule
+
+module RisingEdge_DFlipFlop(input D, input clk,output reg Q);
+always @(posedge clk) 
+begin
+ Q <= D; 
+end 
+endmodule 
