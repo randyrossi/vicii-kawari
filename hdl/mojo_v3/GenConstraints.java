f40c63321f864a438de72ffd96f40897962a6329
ise user <ise@localhost.localdomain>
2020-10-31 04:36:57 +0000

    Update gen constraints tool for quietio

--- hdl/mojo_v3/GenConstraints.java
+++ hdl/mojo_v3/GenConstraints.java
@@ -22,11 +22,12 @@ public class GenConstraints
 		String t5 = st.nextToken();
 		String t6 = st.nextToken();
 		String t7 = st.nextToken();
+		String t8 = st.nextToken();
 
 		System.out.println("NET \""+t6+"\" LOC="+t4+";");
                 if (t7.equals("O") || t7.equals("IO")) {
 		   System.out.println("NET \""+t6+"\" DRIVE=4;");
-		   System.out.println("NET \""+t6+"\" SLOW;");
+		   System.out.println("NET \""+t6+"\" "+t8+";");
                 }
 		if (map.get(t4) != null) { System.out.println("PIN USED TWICE " + t4); System.exit(0);}
 		map.put(t4,1);
