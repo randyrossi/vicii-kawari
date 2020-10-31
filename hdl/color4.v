ef81ddaaded3bcfd954b210814bd226df6b4f57b
ise user <ise@localhost.localdomain>
2020-10-31 19:43:22 +0000

    Use only upper 3 bits for 4 bit color
    Due to excessive noise causing loss of sync
    Need to investigate whether this is something we
    can fix later.

--- hdl/color4.v
+++ hdl/color4.v
@@ -10,23 +10,26 @@ module color4(
            output reg [3:0] blue);
 
     always @*
+	// NOTE: Bit 0 is alway off due to excessive switching noise
+	// causing loss of sync over HDMI.  Temporarily using only
+	// upper 3 bits.
         case (out_pixel)
         `BLACK:{red, green, blue} = {4'h00, 4'h00, 4'h00 };
         `WHITE:{red, green, blue} = {4'h0e, 4'h0e, 4'h0e };
-        `RED:{red, green, blue} = {4'h06, 4'h03, 4'h02 };
-        `CYAN:{red, green, blue} = {4'h07, 4'h0a, 4'h0b };
-        `PURPLE:{red, green, blue} = {4'h06, 4'h03, 4'h08 };
-        `GREEN:{red, green, blue} = {4'h05, 4'h08, 4'h04 };
-        `BLUE:{red, green, blue} = {4'h03, 4'h02, 4'h07 };
-        `YELLOW:{red, green, blue} = {4'h0b, 4'h0c, 4'h06 };
+        `RED:{red, green, blue} = {4'h06, 4'h02, 4'h02 };
+        `CYAN:{red, green, blue} = {4'h06, 4'h0a, 4'h0a };
+        `PURPLE:{red, green, blue} = {4'h06, 4'h02, 4'h08 };
+        `GREEN:{red, green, blue} = {4'h04, 4'h08, 4'h04 };
+        `BLUE:{red, green, blue} = {4'h02, 4'h02, 4'h06 };
+        `YELLOW:{red, green, blue} = {4'h0a, 4'h0c, 4'h06 };
         `ORANGE:{red, green, blue} = {4'h06, 4'h04, 4'h02 };
-        `BROWN:{red, green, blue} = {4'h04, 4'h03, 4'h00 };
-        `PINK:{red, green, blue} = {4'h09, 4'h06, 4'h05 };
+        `BROWN:{red, green, blue} = {4'h04, 4'h02, 4'h00 };
+        `PINK:{red, green, blue} = {4'h08, 4'h06, 4'h04 };
         `DARK_GREY:{red, green, blue} = {4'h04, 4'h04, 4'h04 };
         `GREY:{red, green, blue} = {4'h06, 4'h06, 4'h06 };
-        `LIGHT_GREEN:{red, green, blue} = {4'h09, 4'h0d, 4'h08 };
-        `LIGHT_BLUE:{red, green, blue} = {4'h06, 4'h05, 4'h0b };
-        `LIGHT_GREY:{red, green, blue} = {4'h09, 4'h09, 4'h09 };
+        `LIGHT_GREEN:{red, green, blue} = {4'h08, 4'h0c, 4'h08 };
+        `LIGHT_BLUE:{red, green, blue} = {4'h06, 4'h04, 4'h0a };
+        `LIGHT_GREY:{red, green, blue} = {4'h08, 4'h08, 4'h08 };
       endcase
     
 endmodule: color4
