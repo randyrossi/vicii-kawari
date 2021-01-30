b8391e734748709bedd7eaf5c9e9f212545a02b7
Randy Rossi <randy.rossi@gmail.com>
2021-01-30 13:20:27 -0500

    Add simple config utility

---
+++ prg/BasicGen.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+// Simple pre-parser to handle goto and gosub labels
+// Lables must be prefixed with l_
+public class BasicGen
+{
+     public static void main(String args[]) throws Exception {
+	// Remember labels to line numbers
+	HashMap<String,Integer> map = new HashMap<String,Integer>();
+        File f = new File(args[0]);
+        FileInputStream fis = new FileInputStream(f);
+        InputStreamReader ir = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(ir);
+	int ln = 10;
+        while (true) {
+                String l = br.readLine();
+                if (l == null) break;
+                if (l.indexOf("rem l_") >=0) {
+		   StringTokenizer st = new StringTokenizer(l," ");
+		   String rem = st.nextToken();
+		   String label = st.nextToken();
+		   map.put(label, ln);
+		}
+                if (l.length() == 0) continue;
+		ln+=10;
+        }
+
+	// Open again
+        fis = new FileInputStream(f);
+        ir = new InputStreamReader(fis);
+        br = new BufferedReader(ir);
+	ln = 10;
+        while (true) {
+                String l = br.readLine();
+                if (l == null) break;
+		for (String lab: map.keySet()) {
+			l=l.replaceAll(lab,""+map.get(lab));
+		}
+                if (l.length() == 0) continue;
+		System.out.println(ln + " " + l); ln+=10;
+        }
+
+     }
+}
