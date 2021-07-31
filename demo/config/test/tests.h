bdea668c6a3629cb32ce20fba50d5b141ecf76f8
Randy Rossi <randy.rossi@gmail.com>
2021-07-31 07:41:31 -0400

    Add test case

--- demo/config/test/tests.h
+++ demo/config/test/tests.h
@@ -1,5 +1,6 @@
 
 #define RUN_TEST(VAR) printf ("%s:", #VAR); if (VAR()) { printf (" FAIL\n"); } else { printf ("PASS\n"); }
+#define RUN_TEST1(VAR,ARG) printf ("%s(%s):", #VAR,#ARG); if (VAR(ARG)) { printf (" FAIL\n"); } else { printf ("PASS\n"); }
 
 #define EXPECT_EQ(have, need) if (have != need) {printf (#have "!=" #need); return 1; }
 
@@ -20,3 +21,7 @@ int vmem_a(void);
 int vmem_b(void);
 int vmem_idx_a(void);
 int vmem_idx_b(void);
+
+int vmem_copy(int dir);
+int vmem_copy_overlap(void);
+int vmem_fill(void);
