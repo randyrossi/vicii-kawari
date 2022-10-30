1e8ecb04210a2cf692b39458e5ecf78f48576203
Randy Rossi <randy.rossi@gmail.com>
2022-10-30 00:36:20 +0000

    Fix mcs programming cmds

--- boards/xilinx/kawari_progspi.cmd
+++ boards/xilinx/kawari_progspi.cmd
@@ -5,6 +5,6 @@ setCable -port auto
 Identify
 IdentifyMPM
 attachflash -position 1 -spi "w25q16v"
-assignfiletoattachedflash -position 1 -file "build/spix4_MultiBoot_%BOARD%%VARIANT%_%GOLDEN_MAJOR%_%GOLDEN_MINOR%-%MULTIBOOT_MAJOR%_%MULTIBOOT_MINOR%.mcs"
+assignfiletoattachedflash -position 1 -file "build/spix4_MultiBoot_%VARIANT%_%GOLDEN_MAJOR%_%GOLDEN_MINOR%-%MULTIBOOT_MAJOR%_%MULTIBOOT_MINOR%.mcs"
 program -p 1 -dataWidth 4 -spionly -e -v -loadfpga
 Quit
