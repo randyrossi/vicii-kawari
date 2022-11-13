9c4aaa2507bb06d830310729a38c745f8ae31731
Randy Rossi <randy.rossi@gmail.com>
2022-11-13 12:00:25 -0500

    Make timing summary log file in build for 4H and 4G

--- boards/rev_4G/build.sh
+++ boards/rev_4G/build.sh
@@ -5,7 +5,14 @@ MAJ=1
 MIN=8
 
 VERSION_MAJOR=${MAJ} VERSION_MINOR=${MIN} VARIANT=MAINLG make clean all > build/MAINLG.${MAJ}.${MIN}.log
+cat outflow/vicii.timing.rpt | grep Slack -B4 > build/MAINLG.timing.txt
+
 VERSION_MAJOR=${MAJ} VERSION_MINOR=${MIN} VARIANT=DOTCLG make clean all > build/DOTCLG.${MAJ}.${MIN}.log
+cat outflow/vicii.timing.rpt | grep Slack -B4 > build/DOTCLG.timing.txt
+
 VERSION_MAJOR=${MAJ} VERSION_MINOR=${MIN} VARIANT=MKIILG make clean all > build/MKIILG.${MAJ}.${MIN}.log
+cat outflow/vicii.timing.rpt | grep Slack -B4 > build/MKIILG.timing.txt
+
 VERSION_MAJOR=${MAJ} VERSION_MINOR=${MIN} VARIANT=SARULG make clean all > build/SARULG.${MAJ}.${MIN}.log
+cat outflow/vicii.timing.rpt | grep Slack -B4 > build/SARULG.timing.txt
 
--- boards/rev_4H/build.sh
+++ boards/rev_4H/build.sh
@@ -5,6 +5,14 @@ MAJ=1
 MIN=8
 
 VERSION_MAJOR=${MAJ} VERSION_MINOR=${MIN} VARIANT=MAINLH make clean all > build/MAINLH.${MAJ}.${MIN}.log
+cat outflow/vicii.timing.rpt | grep Slack -B4 > build/MAINLH.timing.txt
+
 VERSION_MAJOR=${MAJ} VERSION_MINOR=${MIN} VARIANT=DOTCLH make clean all > build/DOTCLH.${MAJ}.${MIN}.log
+cat outflow/vicii.timing.rpt | grep Slack -B4 > build/DOTCLH.timing.txt
+
 VERSION_MAJOR=${MAJ} VERSION_MINOR=${MIN} VARIANT=MKIILH make clean all > build/MKIILH.${MAJ}.${MIN}.log
+cat outflow/vicii.timing.rpt | grep Slack -B4 > build/MKIILH.timing.txt
+
 VERSION_MAJOR=${MAJ} VERSION_MINOR=${MIN} VARIANT=SARULH make clean all > build/SARULH.${MAJ}.${MIN}.log
+cat outflow/vicii.timing.rpt | grep Slack -B4 > build/SARULH.timing.txt
+
