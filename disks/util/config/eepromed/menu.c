e7098397b5773df9505c7ccaff9ee4ddd2a54440
Randy Rossi <randy.rossi@gmail.com>
2023-02-01 21:30:30 -0500

    Get eepromed compiling again after flash common lib

--- disks/util/config/eepromed/menu.c
+++ disks/util/config/eepromed/menu.c
@@ -8,6 +8,7 @@
 #include "util.h"
 #include "kawari.h"
 #include "menu.h"
+#include "flash.h"
 
 // SPI_REG - Used for both direct (slow) access to SPI devices
 //           and bulk SPI operation for Kawari to execute.
@@ -47,8 +48,6 @@
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
 
-#define SET(val) asm("lda #"STRINGIFY(val)); asm ("sta $d034");
-
 #define DEVICE_ID_INSTR       0x83
 #define READ_INSTR            0x03
 #define WREN_INSTR            0x06
@@ -64,8 +63,6 @@
 #define TO_TRY_AGAIN 2
 #define TO_NOTHING 3
 
-unsigned char data_out[32];
-unsigned char data_in[32];
 unsigned char data_all[1024];
 
 #define SCRATCH_SIZE 32
@@ -215,160 +212,6 @@ void press_any_key(int code) {
    WAITKEY;
 }
 
-// Helper to read 8 bits from SPI device
-void read8() {
-   unsigned char b;
-   unsigned char value = 0;
-   unsigned int bit;
-   for (b=128;b>=1;b=b/2) {
-        SET(D0_C0_S0);
-        SET(D0_C1_S0);
-        bit = PEEK(SPI_REG) & 1;
-        if (bit)
-	   value = value + b;
-   }
-   data_in[0] = value;
-}
-
-// Generic routine to talk to SPI device
-// 8-bit instruction
-// optional 16 bit address
-// write_count num bytes to write (from data[] array)
-// read_count num bytes to read (into data[] array)
-void talk(unsigned char instr,
-          unsigned char with_addr, unsigned long addr,
-	  unsigned int read_count, unsigned int write_count,
-	  unsigned char close)
-{
-    unsigned char value;
-    unsigned long b;
-    int n;
-    unsigned long bit;
-
-    asm("sei");
-    SET(D1_C1_S1);
-    SET(D0_C1_S0);
-
-    // 8 bit instruction
-    for (b=128L;b>=1L;b=b/2L) {
-       bit = instr & b;
-       if (bit) {
-	       SET(D1_C0_S0);
-               SET(D1_C1_S0);
-       } else {
-	       SET(D0_C0_S0);
-               SET(D0_C1_S0);
-       }
-    }
-
-    // Should we shift a 16 bit address now?
-    if (with_addr) {
-       // 16 bit address
-       for (b=32768L;b>=1L;b=b/2L) {
-          bit = addr & b;
-          if (bit) {
-	       SET(D1_C0_S0);
-               SET(D1_C1_S0);
-          } else {
-	       SET(D0_C0_S0);
-               SET(D0_C1_S0);
-          }
-       }
-    }
-
-    for (n=0;n<write_count;n++) {
-       for (b=128L;b>=1L;b=b/2L) {
-          bit = data_out[n] & b;
-          if (bit) {
-	       SET(D1_C0_S0);
-               SET(D1_C1_S0);
-          } else {
-	       SET(D0_C0_S0);
-               SET(D0_C1_S0);
-          }
-       }
-    }
-
-    // count is num bytes to read
-    for (n=0;n<read_count;n++) {
-       // 8 bit data
-       value = 0;
-       for (b=128L;b>=1L;b=b/2L) {
-	   SET(D0_C0_S0);
-	   SET(D0_C1_S0);
-           bit = PEEK(SPI_REG) & 1;
-           if (bit)
-	      value = value + b;
-       }
-       data_in[n] = value;
-    }
-
-    if (close) {
-       SET(D0_C1_S0); // leave clock high before S high
-       SET(D1_C1_S1); // drive S high again
-    }
-
-    asm("cli");
-}
-
-// Wait for flash device to be ready
-void wait_busy(void) {
-    // INSTR + 1 BYTE READ + NO CLOSE
-    talk(READ_STATUS1_INSTR,
-	0, 0L /* no addr */,
-	1 /* read 1 */,
-	0 /* write 0 */,
-	0 /* no close */);
-
-    while (data_in[0] & 1) {
-       read8();
-    }
-
-    // close
-    SET(D0_C0_S0);
-    SET(D1_C1_S1);
-}
-
-// Read the flash device id bytes
-void read_device(void) {
-    // INSTR + 16 bit 0 + 3 READ BYTES + CLOSE
-    talk(DEVICE_ID_INSTR,
-         1 /* withaddr */, 0,
-	 3 /* read 3 */,
-	 0 /* write 0 */,
-	 1 /* close */);
-}
-
-void read_page(unsigned long addr) {
-    // INSTR + 16 bit addr + 32 read bytes + CLOSE
-    talk(READ_INSTR,
-	1 /* withaddr */, addr,
-	32 /* read 32 */,
-	0 /* write 0 */,
-	1 /* close */);
-}
-
-void write_page(unsigned long addr) {
-    // INSTR + 16 bit addr + 32 write bytes + CLOSE
-    talk(WRITE_INSTR,
-	1 /* withaddr */, addr,
-	0 /* read 0 */,
-	32 /* write 32 */,
-	1 /* close */);
-
-    wait_busy();
-}
-
-// Write enable
-void wren(void) {
-    // INSTR + CLOSE
-    talk(WREN_INSTR,
-	0, 0L /* no addr */,
-	0 /* read 0 */,
-	0 /* write 0 */,
-	1 /* close */);
-}
-
 unsigned long input_string(void) {
    unsigned n = 0;
    for (;;) {
@@ -504,32 +347,6 @@ void verify(void)
 }
 
 
-void write_byte(void)
-{
-   unsigned long addr;
-   unsigned char value;
-
-   printf ("Location:");
-   addr = input_string(); // no type checking on this
-   printf ("\nValue:");
-   value = input_string(); // no type checking on this
-   printf ("\nWrite %d to %d? ", addr, value);
-   WAITKEY;
-   printf ("\n");
-   if (r.a == 'y') {
-      wren();
-      data_out[0] = value;
-      talk(WRITE_INSTR,
-	1 /* withaddr */, addr,
-	0 /* read 0 */,
-	1 /* write 1 */,
-	1 /* close */);
-      wait_busy();
-   } else {
-      printf ("Abort\n");
-   }
-}
-
 // Read the id page into data_in
 void read_id_page(void) {
     // INSTR + 16 bit 0 + 32 READ BYTES + CLOSE
@@ -643,6 +460,25 @@ void get_lock_status(void) {
   if (read_id_lock()) printf ("LOCKED\n"); else printf ("UNLOCKED\n");
 }
 
+void write_a_byte(void)
+{
+   unsigned long addr;
+   unsigned long value;
+
+   printf ("Location:");
+   addr = input_string(); // no type checking on this
+   printf ("\nValue:");
+   value = input_string(); // no type checking on this
+   printf ("\nWrite %ld to %ld? ", value, addr);
+   WAITKEY;
+   printf ("\n");
+   if (r.a == 'y') {
+      write_byte(addr, (char)value);
+   } else {
+      printf ("Abort\n");
+   }
+}
+
 void main_menu(void)
 {
     clrscr();
@@ -659,6 +495,8 @@ void main_menu(void)
     POKE(SPI_REG, 80);
     POKE(SPI_REG, 73);
 
+    use_device(DEVICE_TYPE_EEPROM);
+
     read_device();
 
     printf ("MF=%02x SPI_FAM=%02x DENSITY=%02x\n\n",
@@ -672,7 +510,7 @@ void main_menu(void)
        else if (r.a == 'r') { read_all(); }
        else if (r.a == 'e') { erase_all(); }
        else if (r.a == 'v') { verify(); }
-       else if (r.a == 'w') { write_byte(); }
+       else if (r.a == 'w') { write_a_byte(); }
        else if (r.a == 'i') { show_id_page(); }
        else if (r.a == 'g') { show_serial(); }
        else if (r.a == 's') { set_serial(); }
