81a35a3f1ec9f5655da86041a77307320376750b
Randy Rossi <randy.rossi@gmail.com>
2023-02-02 22:22:52 -0500

    Better variant naming convention
    Trying to standardize code names. Variants should be unique so they show up on CONFIG as different builds.
    
    MDVILG - large dvi
    MRGBLG - large rgb
    MAINLH - mini regular
    DOTCLH - mini with dotc mod

--- boards/rev_4G/build.sh
+++ boards/rev_4G/build.sh
@@ -4,7 +4,7 @@ mkdir -p build
 MAJ=1
 MIN=14
 
-ALL="MAINLG_DVI MAINLG_RGB"
+ALL="MDVILG MRGBLG"
 for V in $ALL
 do
    VERSION_MAJOR=${MAJ} VERSION_MINOR=${MIN} VARIANT=${V} make clean all > build/${V}.${MAJ}.${MIN}.log
