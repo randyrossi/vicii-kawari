aca9bbe15877e6857a457f4b39baf881c8c364b0
Randy Rossi <randy.rossi@gmail.com>
2023-03-12 18:12:07 -0400

    Add sweep arg to build scripts (efinix)

--- boards/rev_4H/build.sh
+++ boards/rev_4H/build.sh
@@ -2,12 +2,17 @@
 mkdir -p build
 
 MAJ=1
-MIN=14
+MIN=15
 
 ALL="MAINLH MAINLH-DOTC"
 
 for V in $ALL
 do
-    VERSION_MAJOR=${MAJ} VERSION_MINOR=${MIN} VARIANT=${V} make clean all > build/${V}.${MAJ}.${MIN}.log
-    cp outflow/vicii.timing.rpt build/${V}.${MAJ}.${MIN}.rpt
+   if [ "$1" = "sweep" ]
+   then
+      VERSION_MAJOR=${MAJ} VERSION_MINOR=${MIN} VARIANT=${V} make clean sweep > build/${V}.${MAJ}.${MIN}_sweep.log
+   else
+      VERSION_MAJOR=${MAJ} VERSION_MINOR=${MIN} VARIANT=${V} make clean all > build/${V}.${MAJ}.${MIN}.log
+      cp outflow/vicii.timing.rpt build/${V}.${MAJ}.${MIN}.rpt
+   fi
 done
